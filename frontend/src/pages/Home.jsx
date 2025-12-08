e39ce4ca079947fcade08879e10f73d0ee6c0a80
pavithiraa <hrpavithraa2000@gmail.com>
2025-12-08 22:47:39 +0530

    features and Testimonial created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Banner from "../components/home/Banner";
 import HeroCopy from "../components/home/HeroContainer";
+import Features from "../components/home/features";
+import Testimonial from "../components/home/Testimonial";
 
 const Home = () => {
   return (
@@ -8,6 +10,8 @@ const Home = () => {
       <Banner />
 
       <HeroCopy />
+      <Features />
+      <Testimonial />
     </div>
   );
 };
